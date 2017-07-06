3e69ec1947220f4b358d450a774c46ecf4e8132e
Kacper Pluta <kacper.pluta@esiee.fr>
2017-07-06 14:11:03 +0200

    Almost all missing function added. Still in debuggig stage.

--- ExtendedForwardAlgorithm.m
+++ ExtendedForwardAlgorithm.m
@@ -112,6 +112,93 @@ TestTranslationVector[t_] := (
 
 (*
 
+Procedure: GetNonInjectiveRegion
+
+Summary:
+  This procedure returns a non-injective region in the remainder range.
+
+Parameters:
+  index -- an index of given non-injective region where 1 - up, 2 - right, 3 - down and 4 left. For
+  details look into a paper cited in :Summary: of this package.  
+  angle -- rotation angle
+
+Output:
+  A non-injective region of a given index.
+
+*)
+GetNonInjectiveRegion[index_, angle_] := (
+  (*up*)
+  If[index == 1, Return[Rectangle[{Sin[angle] - 1/2, -1/2}, {1/2, 1/2 - Cos[angle]}]]];
+  (*right*)
+  If[index == 2, Return[Rectangle[{-1/2, -1/2}, {1/2 - Cos[angle], 1/2 - Sin[angle]}]]];
+  (*down*)
+  If[index == 3, Return[Rectangle[{-1/2, Cos[angle] - 1/2}, {1/2 - Sin[angle], 1/2}]]];
+  (*left*)
+  If[index == 4, Return[Rectangle[{Cos[angle] - 1/2, Sin[angle] - 1/2}, {1/2, 1/2}]]]
+); (* end of GetNonInjectiveRegion *)
+
+
+(*
+
+Procedure: RemainderMap
+
+Summary:
+  This procedure implements the remainder map.
+
+Parameters:
+  angle -- rotation angle
+  t     -- 2D translation vector
+  x     -- integer point
+
+Output:
+  A non-injective region of a given index.
+
+*)
+RemainderMap[angle_, t_, x_] := Module[{U},
+  TestTranslationVector[t];
+  TestTranslationVector[x];
+  U = RotationMatrix[angle].x + t;
+  Return[U - Floor[U + {1/2, 1/2}]];
+];
+
+
+(*
+
+Procedure: CheckInjectivity
+
+Summary:
+  This procedure returns a subset of input set such that each point of such subset induce
+  non-injectivty.
+
+Parameters:
+  p -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
+  q -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
+  t -- 2D translation vector
+  set -- a finite set of integer points
+
+Output:
+  A non-injective region of a given index.
+
+*)
+CheckInjectivity[p_, q_, t_, set_] := Module[{angle, xp, B, F1, F2},
+  TestPythagoreanTripleGenerators[p, q];
+  (*primitive Pythagorean triple*)
+  angle = ArcCos[(2 p q)/(p^2 + q^2)];
+  B = {};
+  F1 = GetNonInjectiveRegion[1, angle];
+  F2 = GetNonInjectiveRegion[2, angle];
+  
+  Do[
+    xp = RemainderMap[angle, t, x];
+    If[xp \[Element] F1 && MemberQ[set, x + {0,1}], AppendTo[B, x]; AppendTo[B, x + {0,1}]];
+    If[xp \[Element] F2 && MemberQ[set, x + {1,0}], AppendTo[B, x]; AppendTo[B, x + {1,0}]];
+  ,{x, set}];
+  Return[B];
+]; (* end of CheckInjectivity *)
+
+
+(*
+
 Procedure: TestHingeAngle
 
  Summary:
@@ -221,7 +308,7 @@ CompareHingeHinge[h_, g_, t_] := Module[{A, B, C, D, l, ll, den},
     ll = Sqrt[g[[1]]^2 + g[[2]]^2 - (g[[3]] - t[[1]] + 1/2)^2];
     den = Denominator[t[[1]]];
    ];
-   If[Im[l] != 0 || Im[ll] != 0, Throw["On of the angles is not valid: a half-grid index is too high!"];];
+   If[Im[l] != 0 || Im[ll] != 0, Print[h, ", ", g]; Throw["On of the angles is not valid: a half-grid index is too high!"];];
    If[Positive[den (A - B)] && Negative[2 den (C ll - D l)], Return[True];];
    If[Negative[den (A - B)] && Positive[2 den (C ll - D l)], Return[False];];
    If[Positive[den (A - B)] && Positive[2 den (C ll - D l)],
@@ -231,103 +318,9 @@ CompareHingeHinge[h_, g_, t_] := Module[{A, B, C, D, l, ll, den},
    ];
    If[Negative[den (A - B)] && Negative[2 den (C ll - D l)],
    If[Positive[4 den^2 (C ll - D l)^2 - den^2 (A^2 + B^2)], 
-     Return[True],
-     Return[TrueQ[4 den^4 A^2 B^2 > den^4 (4 (C ll - D l)^2 - (A^2 + B^2))^2]];];
+     Return[True], Return[TrueQ[4 den^4 A^2 B^2 > den^4 (4 (C ll - D l)^2 - (A^2 + B^2))^2]];];
    ];
 ]; (* end of CompareHingeHinge *)
-
-
-(*
-
-Procedure: GetNonInjectiveRegion
-
-Summary:
-  This procedure returns a non-injective region in the remainder range.
-
-Parameters:
-  index -- an index of given non-injective region where 1 - up, 2 - right, 3 - down and 4 left. For
-  details look into a paper cited in :Summary: of this package.  
-  p -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
-  q -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
-
-Output:
-  A non-injective region of a given index.
-
-*)
-GetNonInjectiveRegion[index_, p_, q_] := Module[{a, b, c},
-  TestPythagoreanTripleGenerators[p, q];
-  (*primitive Pythagorean triple*)
-  b = 2 p q; a = p^2 - q^2; c = p^2 + q^2;
-  (*up*)
-  If[index == 1, Return[Rectangle[{b/c - 1/2, -1/2}, {1/2, 1/2 - a/c}]]];
-  (*right*)
-  If[index == 2, Return[Rectangle[{-1/2, -1/2}, {1/2 - a/c, 1/2 - b/c}]]];
-  (*down*)
-  If[index == 3, Return[Rectangle[{-1/2, a/c - 1/2}, {1/2 - b/c, 1/2}]]];
-  (*left*)
-  If[index == 4, Return[Rectangle[{a/c - 1/2, b/c - 1/2}, {1/2, 1/2}]]]
-]; (* end of GetNonInjectiveRegion *)
-
-
-(*
-
-Procedure: RemainderMap
-
-Summary:
-  This procedure implements the remainder map.
-
-Parameters:
-  p -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
-  q -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
-  t -- 2D translation vector
-  x -- integer point
-
-Output:
-  A non-injective region of a given index.
-
-*)
-RemainderMap[p_, q_, t_, x_] := Module[{a, b, c, U},
-  TestPythagoreanTripleGenerators[p, q];
-  TestTranslationVector[t];
-  TestTranslationVector[x];
-  (*primitive Pythagorean triple*)
-  b = 2 p q; a = p^2 - q^2; c = p^2 + q^2;
-  U = {{a/c, -b/c},{b/c, a/c}}.x + t;
-  Return[U - Round[U]];
-];
-
-
-(*
-
-Procedure: CheckInjectivity
-
-Summary:
-  This procedure returns a subset of input set such that each point of such subset induce
-  non-injectivty.
-
-Parameters:
-  p -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
-  q -- generator of primitive Pythagorean triple such that p - q is odd and gcd of p and q is 1.
-  t -- 2D translation vector
-  set -- a finite set of integer points
-
-Output:
-  A non-injective region of a given index.
-
-*)
-CheckInjectivity[p_, q_, t_, set_] := Module[{xp, B, F1, F2},
-  B = {};
-  F1 = GetNonInjectiveRegion[1, p, q];
-  F2 = GetNonInjectiveRegion[2, p, q];
-  
-  Do[
-    xp = RemainderMap[p, q, t, x];
-    If[xp \[Element] F1 && MemberQ[set, x + {0,1}], AppendTo[B, x]; AppendTo[B, x + {0,1}]];
-    If[xp \[Element] F2 && MemberQ[set, x + {1,0}], AppendTo[B, x]; AppendTo[B, x + {1,0}]];
-  ,{x, set}];
-  Return[B];
-]; (* end of CheckInjectivity *)
-
 
 
 (*
@@ -357,14 +350,144 @@ Get4Neighborhod[set_, x_] := Module[{N},
  Return[N];
 ]; 
 
+ 
+(*
 
+Procedure: HingeCos
 
-FindInjectiveRanges[p_, q_, t_, set_] := Module[{C},
- Do[
-  If[Length[CheckInjectivity[p_, q_, t_, Get4Neighborhod[set, x]]] != 0, Return[{}]]; 
-  
- , {x, set}
+ Summary:
+  This procedure computes cosine of a hinge angle
+
+Parameters:
+  h -- 4D vector with integer elements. The format is {p_1, p_2, k, s}.
+  t -- 2D vector with rational elements.
+
+Output:
+  True if the Pythagorean angle is bigger than the hinge angle and false otherwise. 
+*)
+HingeCos[h_, t_] := Module[{l},
+ TestTranslationVector[t]; TestHingeAngle[h];
+ If[h[[4]] == 1, 
+  l = Sqrt[h[[1]]^2 + h[[2]]^2 - (h[[3]] - t[[2]] + 1/2)^2];
+  If[Im[l] != 0, Throw["The angle is not valid: a half-grid index is too high!"];];
+  Return[(h[[1]] l + h[[2]] (h[[3]] - t[[2]] + 1/2))/(h[[1]]^2 + h[[2]]^2)];,
+  l = Sqrt[h[[1]]^2 + h[[2]]^2 - (h[[3]] - t[[1]] + 1/2)^2];
+  If[Im[l] != 0, Throw["The angle is not valid: a half-grid index is too high!"];];
+  Return[(h[[2]] l + h[[1]] (h[[3]] - t[[1]] + 1/2))/(h[[1]]^2 + h[[2]]^2)];
  ];
+];
+
+
+(*
+
+Procedure: ClosestUpperHinge
+
+ Summary:
+  This procedure finds closest upper hinge angle.
+
+Parameters:
+  p     -- An integer point
+  angle -- A Pythagorean or a hinge angle
+  t     -- 2D vector with rational elements.
+
+Output:
+  The closest upper hinge angle 
+*)
+ClosestUpperHinge[p_, angle_, t_] := Module[{x},
+ TestTranslationVector[t];
+ If[Length[angle] == 3,
+  x = Floor[RotationMatrix[ArcCos[angle[[1]]/angle[[3]]]].p + t + {1/2, 1/2}];,
+  x = Floor[RotationMatrix[ArcCos[HingeCos[angle, t]]].p + t + {1/2, 1/2}];
+ ];
+ Print[x];
+ If[x[[1]] > 0 && x[[2]] >= 0,
+  If[CompareHingeHinge[{p[[1]], p[[2]], x[[1]] - 1, 0}, {p[[1]], p[[2]], x[[2]], 1}, t],
+   Return[{p[[1]], p[[2]], x[[1]] - 1, 0}];, Return[{p[[1]], p[[2]], x[[2]], 1}];
+  ];
+ ];
+ If[x[[1]] <= 0 && x[[2]] > 0,
+  If[CompareHingeHinge[{p[[1]], p[[2]], x[[1]] - 1, 0}, {p[[1]], p[[2]], x[[2]] - 1, 1}, t],
+   Return[{p[[1]], p[[2]], x[[1]] - 1, 0}];, Return[{p[[1]], p[[2]], x[[2]] - 1, 1}];
+  ];
+ ];
+ If[x[[1]] < 0 && x[[2]] <= 0,
+  If[CompareHingeHinge[{p[[1]], p[[2]], x[[1]], 0}, {p[[1]], p[[2]], x[[2]] - 1, 1}, t],
+   Return[{p[[1]], p[[2]], x[[1]], 0}];, Return[{p[[1]], p[[2]], x[[2]] - 1, 1}];
+  ];
+ ];
+ If[x[[1]] >= 0 && x[[2]] < 0,
+  If[CompareHingeHinge[{p[[1]], p[[2]], x[[1]], 0}, {p[[1]], p[[2]], x[[2]], 1}, t],
+   Return[{p[[1]], p[[2]], x[[1]], 0}];, Return[{p[[1]], p[[2]], x[[2]], 1}];
+  ];
+ ];
+];
+
+
+
+(*
+
+Procedure: CompareAngles
+
+ Summary:
+  This procedure compares two angles which are either two (resp. one) hinge or one hinge and one
+  Pythagorean angles. 
+
+Parameters:
+  h  -- 4D vector with integer elements. The format is {p_1, p_2, k, s}.
+  g  -- A Pythagorean or a hinge angle given as a 4D (or 3D) integer vector
+  t  -- 2D vector with rational elements.
+
+Output:
+  True if the second angle is bigger than the first one and False otherwise.
+*)
+CompareAngles[h_, g_, t_] := (
+ TestTranslationVector[t];
+ If[Length[g] == 3,
+  Return[CompareHingePythagorean[h, g, t]];, Return[CompareHingeHinge[h, g, t]];
+ ];  
+);
+
+
+(*
+
+Procedure: ReduceHingeSet
+
+ Summary:
+ This procedure returns hinge angles which are in a range defined by gl and up.
+
+Parameters:
+  B  --  A set of hinge angles
+  gl -- A lower hinge angle
+  gt -- An upper hinge angles
+
+Output:
+  A set of hinge angles in the range defined by gl < gu.
+*)
+ReduceHingeSet[B_, gl_, gu_, t_] := Module[{tmp},
+ tmp = {};
+ Do[If[!CompareAngle[x, gl, t] && CompareAngle[x, gu, t], AppendTo[tmp, x]]; ,{x, B}];
+ Return[tmp];
+];
+
+
+CheckInjectivityRange[p_, q_, t_, set_] := Module[{F1, F2, B, a, b, c, gl, gu, alpha},
+ TestPythagoreanTripleGenerators[p, q]; TestTranslationVector[t];
+ a = 2 p q; b = p^2 - q^2; c = p^2 + q^2;
+ B = {}; gl = {1, 1, 1}; gu = {0, 1, 1};
+ Do[
+  Print[x];
+  If[Length[CheckInjectivity[p, q, t, Get4Neighborhod[set, x]]] != 0, Return[{}]]; 
+   alpha = ClosestUpperHinge[x, {a, b, c}, t];
+   While[!CompareAngle[alpha, gl, t] && CompareAngle[alpha, gu, t], 
+    xp = RemainderMap[alpha, t, x];
+    F1 = GetNonInjectiveRegion[1, alpha]; F2 = GetNonInjectiveRegion[2, alpha];
+    If[(xp \[Element] F1 && MemberQ[set, x + {0,1}]) || (xp \[Element] F2 && MemberQ[set, x + {1,0}]), 
+     gl = alpha;, AppendTo[B, alpha]; alpha = ClosestUpperHinge[x, alpha, t];
+    ];
+   ];
+  B = ReduceHingeSet[B, gl, gu, t];
+ , {x, set}];
+ Return[B];
 ];
 
 
